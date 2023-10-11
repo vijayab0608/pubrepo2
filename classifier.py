19d987eae85e3c112b2f29bf4e60a3a942ca2fd2
vijayab0608 <vijayab0608@gmail.com>
2023-10-11 22:39:50 +0000

    first code

--- classifier.py
+++ classifier.py
@@ -7,7 +7,7 @@ df = pd.read_csv("/content/pubrepo2/data/iris_csv.csv")
 features = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
 target = 'Species'
 
-X_train,  X_test, y_train, y_test = train_test_split(df[features],df[target], test_size=0.3, shuffle=True)
+X_train,  X_test, y_train, y_test = train_test_split(df[features],df[target], test_size=0.4, shuffle=True)
 #step 1 : initialise the model class
 clf = DecisionTreeClassifier(criterion="entropy") #Information gain as criteria
 #step 2 : train the model on training set
